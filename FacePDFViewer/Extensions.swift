640f32184ac7ddf7f4bacfadb0888522afe57615
gwangik.choi <gwangik.choi@linecorp.com>
2019-05-21 10:04:50 +0900

    Test 3 case for getting eye tracking point.

---
+++ FacePDFViewer/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  FacePDFViewer
+//
+//  Created by cgi on 20/05/2019.
+//  Copyright © 2019 cgi. All rights reserved.
+//
+
+
--- FacePDFViewer/ViewController.swift
+++ FacePDFViewer/ViewController.swift
@@ -17,20 +17,41 @@ class ViewController: UIViewController {
     
     //let session = ARSession()
     
+    let virtualPhoneNode = SCNNode()
     var virtualScreenNode: SCNNode = {
-        
         let screenGeometry = SCNPlane(width: 1, height: 1)
         screenGeometry.firstMaterial?.isDoubleSided = true
-        screenGeometry.firstMaterial?.diffuse.contents = UIColor.green
-        
+        screenGeometry.firstMaterial?.diffuse.contents = UIColor.clear
         return SCNNode(geometry: screenGeometry)
     }()
+    
+   
+    var notMovingNode = SCNNode(geometry: SCNSphere(radius: 0.001))
+    var lookPointNode = SCNNode(geometry: SCNSphere(radius: 0.001))
+    
+    let leftEyeNode = SCNNode(geometry: SCNSphere(radius: 0.01))
+    let rightEyeNode = SCNNode(geometry: SCNSphere(radius: 0.01))
+    let midpointNode = SCNNode(geometry: SCNSphere(radius: 0.01))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //sceneView.scene.background.contents = UIColor.clear
+        
+        if !ARFaceTrackingConfiguration.isSupported {
+            print("not supported!!")
+            return
+        }
+        
         sceneView.session.run(ARFaceTrackingConfiguration())
         sceneView.delegate = self
+        
+        sceneView.scene.rootNode.addChildNode(lookPointNode)
+        sceneView.pointOfView?.addChildNode(notMovingNode)
+        notMovingNode.position = SCNVector3(0.001, 0.001, -0.01)
+        sceneView.pointOfView?.addChildNode(virtualPhoneNode)
+        virtualPhoneNode.addChildNode(virtualScreenNode)
+        virtualScreenNode.position = SCNVector3(0, 0, -0.01)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -103,14 +124,18 @@ extension ViewController: ARSessionDelegate {
  */
 
 extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        
+    }
     
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard anchor is ARFaceAnchor else {
-            return nil
+            return
         }
-
-        let node = SCNNode()
-        return node
+        
+        node.addChildNode(leftEyeNode)
+        node.addChildNode(rightEyeNode)
+        node.addChildNode(midpointNode)
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
@@ -118,14 +143,54 @@ extension ViewController: ARSCNViewDelegate {
             return
         }
         
-        // 얼굴 지점과 lookAtPoint사이를 잇는 선과 핸드폰 평면사이의 교점
+        let options : [String: Any] = [SCNHitTestOption.backFaceCulling.rawValue: false,
+                                       SCNHitTestOption.searchMode.rawValue: 1,
+                                       SCNHitTestOption.ignoreChildNodes.rawValue : false,
+                                       SCNHitTestOption.ignoreHiddenNodes.rawValue : false]
+        
+        
+        // 방법1: 얼굴과 lookAtPoint 사이 선과 면의 교점
+        let hitTestLookPoint = virtualPhoneNode.hitTestWithSegment(from: virtualPhoneNode.convertPosition(node.worldPosition, from:nil), to: virtualPhoneNode.convertPosition(SCNVector3(faceAnchor.lookAtPoint), from: node), options: options)
+        
+        // 방법2: 두 눈 사이의 중점과 lookAtPoint사이 선과 면의 교점
+        leftEyeNode.transform = SCNMatrix4(faceAnchor.leftEyeTransform)
+        rightEyeNode.transform = SCNMatrix4(faceAnchor.rightEyeTransform)
+        midpointNode.simdPosition = (leftEyeNode.simdPosition + rightEyeNode.simdPosition) / 2
+        
+        let hitTestLookPoint2 = virtualPhoneNode.hitTestWithSegment(from: virtualPhoneNode.convertPosition(midpointNode.worldPosition, from:nil), to: virtualPhoneNode.convertPosition(SCNVector3(faceAnchor.lookAtPoint), from: node), options: options)
+        
+        /*
+        //방법3: 두 눈에서 뻗어나가는 직선과 면의 교점 2개의 중점(2와 실질적으로 동일)
+        let hitTestLeftEyeLookPoint = virtualPhoneNode.hitTestWithSegment(from: virtualPhoneNode.convertPosition(rightEyeNode.worldPosition, from:nil), to: virtualPhoneNode.convertPosition(rightEyeEndNode.worldPosition, from: nil), options: options)
+        let hitTestRightEyeLookPoint = virtualPhoneNode.hitTestWithSegment(from: virtualPhoneNode.convertPosition(leftEyeNode.worldPosition, from:nil), to: virtualPhoneNode.convertPosition(leftEyeEndNode.worldPosition, from: nil), options: options)
+         */
+        
         count += 1
         if count == 60 {
-            print(node.convertPosition(SCNVector3(faceAnchor.lookAtPoint), to: sceneView.scene.rootNode))
+            if !hitTestLookPoint.isEmpty {
+                print("Case 1")
+                print(hitTestLookPoint[0].localCoordinates)
+                print(renderer.projectPoint(hitTestLookPoint[0].worldCoordinates))
+            }
+            
+            if !hitTestLookPoint2.isEmpty {
+                print("Case 2")
+                print(hitTestLookPoint2[0].localCoordinates)
+                print(renderer.projectPoint(hitTestLookPoint2[0].worldCoordinates))
+            }
+            
+            /*
+            if !hitTestLeftEyeLookPoint.isEmpty,
+                !hitTestRightEyeLookPoint.isEmpty {
+                print("Case 3")
+                let hitTestLookPoint3 = SCNVector3((hitTestLeftEyeLookPoint[0].localCoordinates.x + hitTestRightEyeLookPoint[0].localCoordinates.x) / 2,
+                                                  (hitTestLeftEyeLookPoint[0].localCoordinates.y + hitTestRightEyeLookPoint[0].localCoordinates.y) / 2,
+                                                  (hitTestLeftEyeLookPoint[0].localCoordinates.z + hitTestRightEyeLookPoint[0].localCoordinates.z) / 2)
+                print(hitTestLookPoint3)
+            }
+            */
+            
             count = 0
         }
     }
-    
 }
-
-
